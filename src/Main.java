851b9e30c6ca6edd04a569c27c8fd416fd5d1784
RodrigoHuche <rodrigohuche@gmail.com>
2022-05-26 09:57:22 -0300

    Iconta no Main

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        Conta cc = new ContaCorrente();
+        Iconta cc = new ContaCorrente();
         cc.depositar(100);
 
-        Conta poupanca = new ContaPoupanca();
+        Iconta poupanca = new ContaPoupanca();
         //poupanca.depositar(500);
         cc.transferir(90, poupanca);
 
