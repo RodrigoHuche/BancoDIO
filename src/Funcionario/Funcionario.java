f041b0c15a8d5a17123d897c81383779ba82b63c
RodrigoHuche <rodrigohuche@gmail.com>
2022-05-26 13:32:56 -0300

    New class Funcionario + Lombok

---
+++ src/Funcionario/Funcionario.java
@@ -0,0 +1,12 @@
+package Funcionario;
+
+public class Funcionario {
+
+    public static void main(String[] args) {
+        Funcionariomodel funcinario = new Funcionariomodel();
+        funcinario.setId(10);
+        funcinario.setNome("Paulo Generico");
+        funcinario.setCargo("Gerente");
+    }
+
+}
---
+++ src/Funcionario/Funcionariomodel.java
@@ -0,0 +1,11 @@
+package Funcionario;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class Funcionariomodel {
+    private Integer id;
+    private String nome;
+    private String cargo;
+}
