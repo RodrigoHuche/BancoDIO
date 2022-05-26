2d5c69dc3aac8453dcfb11263579acc8f5643dac
RodrigoHuche <rodrigohuche@gmail.com>
2022-05-25 23:24:14 -0300

    Polimorfismo + encapsulamento com herança

--- src/Conta.java
+++ src/Conta.java
@@ -1,9 +1,32 @@
 //o fato da classe conta ser abstrata, garante não implementação de um new por exe, a não ser os filhos.
-public abstract class Conta implements Iconta{
+public abstract class Conta implements Iconta {
 
-    private int agencia;
-    private int numero;
-    private double saldo;
+    private static final int AGENCIA_PADRAO = 1;
+    private static int Sequencial = 1;
+
+    protected int agencia;
+    protected int numero;
+    protected double saldo;
+
+    public Conta() {
+        this.agencia = Conta.AGENCIA_PADRAO;
+        this.numero = Sequencial++;
+    }
+
+    @Override
+    public void sacar(double valor) {
+
+    }
+
+    @Override
+    public void depositar(double valor, Conta contaDestino) {
+
+    }
+
+    @Override
+    public void transferir(double valor) {
+
+    }
 
     public int getAgencia() {
         return agencia;
@@ -15,20 +38,5 @@ public abstract class Conta implements Iconta{
 
     public double getSaldo() {
         return saldo;
-    }
-
-    @Override
-    public void transferir(double valor) {
-
-    }
-
-    @Override
-    public void sacar(double valor) {
-
-    }
-
-    @Override
-    public void depositar(double valor, Conta contaDestino) {
-
     }
 }
